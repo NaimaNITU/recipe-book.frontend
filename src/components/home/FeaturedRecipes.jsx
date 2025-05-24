8573ad0431b77cdaff9460de4e29d46cbd3228ca
NaimaNITU <toktikproject@gmail.com>
2025-05-24 23:03:25 +0600

    Update on home 3 call and all recipe page 4 call card added

--- src/components/home/FeaturedRecipes.jsx
+++ src/components/home/FeaturedRecipes.jsx
@@ -1,5 +1,5 @@
 import { Link } from "react-router-dom";
-import RecipeList from "../recipe/RecipeList";
+import RecipeList from "../recipe/TopRecipeList";
 
 const FeaturedRecipes = ({ recipes, onLike }) => {
   return (
--- src/components/recipe/TopRecipeList.jsx
+++ src/components/recipe/TopRecipeList.jsx
@@ -45,7 +45,7 @@ const RecipeList = ({
   }
 
   return (
-    <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-4 xl:grid-cols-4 gap-6">
+    <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-3 gap-6">
       {recipes.map((recipe) => (
         <RecipeCard key={recipe._id} recipe={recipe} onLike={handleLike} />
       ))}
